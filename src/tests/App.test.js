7f6a75db88c5dfe01b14f5f870591c3ae2941ac7
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-01-29 20:40:57 -0300

    requisito 5, iniciando testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import PlanetsProvider from '../context/PlanetsProvider';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Inicie a aplicação e...', () =>  {
+  test('se todos os elementos iniciais são renderizados na tela', () => {
+    render(<App />);
+    const nameFilter = screen.getByTestId("name-filter");
+    const columnFilter = screen.getByTestId("column-filter");
+    const comparisonFilter = screen.getByTestId("comparison-filter");
+    const valueFilter = screen.getByTestId("value-filter");
+    const buttonFilter = screen.getByTestId("button-filter");
+
+    expect(nameFilter).toBeInTheDocument();
+    expect(columnFilter).toBeInTheDocument();
+    expect(comparisonFilter).toBeInTheDocument();
+    expect(valueFilter).toBeInTheDocument();
+    expect(buttonFilter).toBeInTheDocument();
+  });
+  //   test('', () => {
+    
+  // });
 });
+
